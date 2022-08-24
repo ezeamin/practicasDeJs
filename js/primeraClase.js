ce68305212723933eee365847b26aab5ff5a7774
Ezequiel Amin <ezequielamin@outlook.com>
2022-08-24 20:26:00 -0300

    Primer commit

---
+++ js/primeraClase.js
@@ -0,0 +1,32 @@
+console.log("Hola mundo");
+document.write("Hola mundo");
+
+//console.log("Este es un 2do mensaje");
+
+const PI=3.14;
+console.log(typeof PI);
+
+let suma = 0;
+let valorDeEntrada = Number(prompt("Indique un valor"));
+console.log( valorDeEntrada );
+console.log(typeof valorDeEntrada);
+
+suma = suma + valorDeEntrada;
+// "0" + "1" = "01"
+
+valorDeEntrada = Number(prompt("Indique un valor"));
+console.log(valorDeEntrada);
+
+suma = suma + valorDeEntrada;
+
+valorDeEntrada = Number(prompt("Indique un valor"));
+console.log(valorDeEntrada);
+
+suma = suma + valorDeEntrada;
+
+valorDeEntrada = Number(prompt("Indique un valor"));
+console.log(valorDeEntrada);
+
+suma = suma + valorDeEntrada;
+
+console.log("La suma es de " + suma);
